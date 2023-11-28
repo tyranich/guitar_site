52b389d6263172e5847bdd700e69949f01b227e0
michael turanov <mturanov@briogroup.ru>
2023-11-28 17:30:45 +0300

    commit test

--- educate_website/settings.py
+++ educate_website/settings.py
@@ -115,9 +115,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_DIRS = [os.path.join(BASE_DIR,"guitarlesson", "static")]
-print(STATIC_DIRS)
+STATIC_URL = '/static/'
+STATIC_DIRS = [os.path.join(BASE_DIR, "static")]
+#print(STATIC_DIRS)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
