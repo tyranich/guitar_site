8222552e78257c87333c8468c008ed055337c8a7
michael turanov <mturanov@briogroup.ru>
2023-12-26 15:00:48 +0300

    correct path to static files

--- educate_website/settings.py
+++ educate_website/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+print("BASE DIR ", BASE_DIR)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -115,10 +115,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_URL = 'guitarlesson/static/'
+#STATIC_DIRS = [os.path.join(BASE_DIR, "guitarlesson\static")]
 #print(STATIC_DIRS)
-
+print(STATIC_URL)
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
